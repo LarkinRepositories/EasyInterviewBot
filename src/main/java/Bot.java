71d40936346e753725a1938647bb3d81929f6197
Dmitriy <larkingit@yandex.ru>
2019-09-17 10:31:49 +0700

    добавлен метод messageHandling

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,9 +1,27 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
+        update.getUpdateId();
+        try {
+            messageHandling(update);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void messageHandling(Update update) throws TelegramApiException {
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        String message = update.getMessage().getText();
+        if (message.contains("Привет")) {
+            sendMessage.setText("Привет, дружище!");
+            execute(sendMessage);
+        }
 
     }
 
