7a50ee7f76216a693796c6c7da70e9eff28de648
Dmitriy <larkingit@yandex.ru>
2019-09-17 10:31:20 +0700

    добавлен registerBot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-
+        Bot easyInterviewBot = new Bot();
+        try {
+            telegramBotsApi.registerBot(easyInterviewBot);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
 
     }
 }
